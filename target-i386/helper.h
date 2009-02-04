352c7b621d26c25ffd38f92e8d18dd163b9066dd
Jason Martens <jmartens@he-man.(none)>
2009-02-03 22:06:22 -0600

    Used wrong version of HELPER

--- target-i386/helper.h
+++ target-i386/helper.h
@@ -205,7 +205,7 @@ DEF_HELPER_0(vmxoff, void)
 DEF_HELPER_1(vmclear, void, tl)
 DEF_HELPER_1(vmptrld, void, tl)
 DEF_HELPER_1(vmptrst, tl, tl)
-DEF_HELPER_1(vmwrite, void, tl, tl)
+DEF_HELPER_2(vmwrite, void, tl, tl)
 DEF_HELPER_1(vmread, tl, tl)
 
 #define SHIFT 0
